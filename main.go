50aea91a231bd1a53cc935db37ad48e57809bc51
Sungjin Han <meinside@gmail.com>
2018-12-05 10:17:45 +0900

    Edit help message

--- main.go
+++ main.go
@@ -39,9 +39,11 @@ Supported commands are as following:
 %s: Summarize current market information.
 %s: Show this help message.
 
-You can search for card info in other chats with:
+You can search for card info in chats with:
 
 *@%s [search keyword]*
+
+- Source code: https://github.com/meinside/telegram-bot-artifact
 `
 	messageHelpKor = `*도움말:*
 
@@ -52,11 +54,13 @@ _스팀 커뮤니티 장터_에서 *Artifact* 정보를 가져오는 Telegram Bo
 %s: 현재 장터 정보를 요약합니다.
 %s: 이 도움말을 표시합니다.
 
-다른 대화창에서
+대화창에서
 
 *@%s [검색어]*
 
 를 입력하여 카드 정보를 바로 조회, 인용할 수 있습니다.
+
+- 소스 코드: https://github.com/meinside/telegram-bot-artifact
 `
 	messageSummaryEng = `*Summary:*
 
